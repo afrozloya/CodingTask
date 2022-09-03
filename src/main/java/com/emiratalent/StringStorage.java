af8c96e77fa908788cdbbfa8e346d21022a07488
Afroz Loya <mloya@accelaero.com>
2022-09-03 11:41:29 +0400

    removed extra class

--- src/main/java/com/emiratalent/StringStorage.java
+++
@@ -1,9 +0,0 @@
-package com.emiratalent;
-
-public class StringStorage {
-//    public void store(String fn, Compressor compressor, Filter filter) {
-//        compressor.compress(fn);
-//        filter.apply(fn);
-//        System.out.println("storing " + fn);
-//    }
-}
